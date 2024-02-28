46d8afe923fe036e624f48a178284b49f3eaf3bb
ctrichet <clement.trichet.pro@gmail.com>
2024-02-28 13:05:52 +0100

    Modification de la classe State en une classe SimulationState, héritant d'une nouvelle classe State. Les attributs de la classe d'origine ont été réparties au sein des classes mère et fille.

--- state.cpp
+++ state.cpp
@@ -1,9 +1,7 @@
 #include <array>
 #include "state.h"
 
-const int State::moves[8] = {0, 1, 1, 1, 0, -1, -1, -1};
-
-State::State() : activePlayer(PLAYER0)
+State::State(bool activePlayer /*=PLAYER0*/) : activePlayer(activePlayer)
 {
     /*Initialisation du plateau*/
     for(unsigned int row = 0; row < WIDTH; ++row)
@@ -27,14 +25,6 @@ State::State() : activePlayer(PLAYER0)
     /*Initialisation des coordonnées du neutron*/
     neutron = {MIDDLE, MIDDLE};
     
-    /*Initialisation des compteurs de progression*/
-    std::fill(progressCounters.begin(), progressCounters.end(), 0);
-}
-
-State::State(bool activePlayer, std::array<std::array<int, WIDTH>, WIDTH> newBoard, std::array<std::array<std:array<unsigned int, BOARD_DIM>, NB_PROTONS>, NB_PLAYERS> newProtons, std::array<unsigned int, BOARD_DIM> newNeutron) : activePlayer(! activePlayer), board(newBoard), protons(newProtons), neutron(newNeutron)
-{
-    /*Initialisation des compteurs de progression*/
-    std::fill(progressCounters.begin(), progressCounters.end(), 0);
 }
 
 State::~State()
@@ -43,89 +33,4 @@ State::~State()
 int State::computeScore()
 {
     //TODO
-}
-
-unsigned int State::moveProton(bool save /*=false*/, bool firstMove /*=false*/) // TODO Variables globales save mais surtout firstMove!!!
-{
-    int rowMove = State::moves[this->progressCounters[PROTON]];
-    int colMove = State::moves[(this->progressCounters[PROTON] + 2) % DIRECTIONS];
-    int row = this->protons[this->activePlayer][this->progressCounters[TARGET]][ROW] + rowMove;
-    int col = this->protons[this->activePlayer][this->progressCounters[TARGET]][COL] + colMove;
-    if(row < FIRST_ROW or row > LAST_ROW or col < FIRST_ROW or col > LAST_ROW or this->board[row][col] != EMPTY)
-    {
-        if(firstMove)
-            return NULL;
-        return changeNeutronDirection(save, firstMove);
-    }
-    //State::proton[OLD][ROW] = this->protons[this->activePlayer][this->progressCounters[TARGET]][ROW];//TODO En une ligne? 
-    //State::proton[OLD][COL] = this->protons[this->activePlayer][this->progressCounters[TARGET]][COL];//TODO
-    do
-    {
-        State::proton/*[NEW]*/[ROW] = row;//TODO
-        State::proton/*[NEW]*/[COL] = col;//TODO
-        row += rowMove;
-        col += colMove; 
-    } while(row > FIRST_ROW & row < LAST_ROW & col > FIRST_ROW & col < LAST_ROW & this->board[row][col] == EMPTY);
-    if(save)
-        saveProtonMove(); //TODO
-    return MOVE;
-}
-
-unsigned int State::moveNeutron(bool save /*=false*/, bool firstMove /*=false*/)
-{
-    int rowMove = State::moves[this->progressCounters[NEUTRON]];
-    int colMove = State::moves[(this->progressCounters[NEUTRON] + 2) % DIRECTIONS];
-    int row = this->neutron[ROW] + rowMove;
-    int col = this->neutron[COL] + colMove;
-    if(row < FIRST_ROW or row > LAST_ROW or col < FIRST_ROW or col > LAST_ROW or this->board[row][col] != EMPTY)
-        return changeNeutronDirection(save, firstMove);
-    //State::neutron[OLD][ROW] = this->neutron[this->progressCounters[NEUTRON]][ROW];//TODO En une ligne? 
-    //State::neutron[OLD][COL] = this->neutron[this->progressCounters[NEUTRON]][COL];//TODO
-    do
-    {
-        State::neutron/*[NEW]*/[ROW] = row;//TODO
-        State::neutron/*[NEW]*/[COL] = col;//TODO
-        row += rowMove;
-        col += colMove;
-    } while(row > FIRST_ROW & row < LAST_ROW & col > FIRST_ROW & col < LAST_ROW & this->board[row][col] == EMPTY);
-    if(save)
-        saveNeutronMove();//TODO
-    if(row % LAST_ROW)
-    {
-        moveProton(save, FirstMove);
-    }
-    return MAT;
-}
-
-unsigned int State::changeProtonDirection(bool save /*=false*/, bool firstMove /*=false*/)
-{
-    if(progressCounters[PROTON] == LAST_DIR)
-    {
-        progressCounters[PROTON] = 0;
-        return changeTargetedProton(save, firstMove);
-    }
-    ++progressCounters[PROTON];
-    return moveProton(save, bool firstMove);
-
-}
-
-unsigned int State::changeNeutronDirection(bool save /*=false*/, bool firstMove /*=false*/)
-{
-    if(progressCounters[NEUTRON] == LAST_DIR)
-        return NULL;
-    ++progressCounters[NEUTRON];
-    return moveNeutron(save, firstMove);
-}
-
-unsigned int State::changeTargetedProton(bool save /*=false*/, bool firstMove /*=false*/)
-{
-    if(progressCounters[TARGET] == LAST_PROTON)
-    {
-        if(firstMove)
-            return NULL;
-        progressCounters[TARGET] = 0;
-        return changeNeutronDirection(save, firstMove);
-    }
-    ++progressCounters[TARGET];
-    return moveProton(save, firstMove);
 }
--- state.h
+++ state.h
@@ -6,52 +6,29 @@
 #define NB_PLAYERS          2
 #define BOARD_DIM           2
 #define WIDTH               5
-#define NB_PROTONS          5
 #define EMPTY              -1
 #define PLAYER0             0
 #define PLAYER1             1
 #define NEUTRON             2
-#define PROGRESS_COUNTERS   3
-#define VERBOSE             1
 #define FIRST_ROW           0
 #define LAST_ROW            4
 #define MIDDLE              2
-#define DIRECTIONS          8
-#define LAST_DIR            7
-#define LAST_PROTON         4
-#define PROTON              1
-#define ROW                 0
-#define COL                 1
-#define TARGET              0
-
-#define MAT                 0
-#define MOVE                1
-#define NULL                2
 
 class State 
 {
   private:
 
-    static const int moves[8];
-
     bool    activePlayer;
     std::array<std::array<int, WIDTH>, WIDTH> board;
     std::array<std::array<std:array<unsigned int, BOARD_DIM>, NB_PROTONS>, NB_PLAYERS> protons;
     std::array<unsigned int, BOARD_DIM> neutron;
-    std::array<unsigned , PROGRESS_COUNTERS> progressCounters;
 
   public:
 
     State();
-    State(bool activePlayer, std::array<std::array<int, WIDTH>, WIDTH> newBoard, std::array<std::array<std:array<unsigned int, BOARD_DIM>, NB_PROTONS>, NB_PLAYERS> newProtons, std::array<unsigned int, BOARD_DIM> newNeutron);
     ~State();
 
-    int computeScore();
-    unsigned int moveProton(bool save = false, bool firstMove = false);
-    unsigned int moveNeutron(bool save = false, bool firstMove = false);
-    unsigned int changeProtonDirection(bool save = false, bool firstMove = false);
-    unsigned int changeNeutronDirection(bool save = false, bool firstMove = false);
-    unsigned int changeTargetedNeutron(bool save = false, bool firstMove = false);
+    int computeScore() const;
     
 };
 
